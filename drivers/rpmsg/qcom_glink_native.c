d523015e6c3a492db52c91e726d1e8218a7f53ae
chaptsand <chaptsand@gmail.com>
2024-03-17 11:25:57 +0800

    drivers/rpmsg: Import SAMSUNG changes
    Change-Id: If0997b37cfb3e59cb4d2cbab15f0436da47dbda5

--- drivers/rpmsg/qcom_glink_native.c
+++ drivers/rpmsg/qcom_glink_native.c
@@ -33,6 +33,11 @@
 #include "qcom_glink_native.h"
 
 #define GLINK_LOG_PAGE_CNT 32
+
+#if IS_ENABLED(CONFIG_SEC_PM)
+#include <linux/wakeup_reason.h>
+#endif
+
 #define GLINK_INFO(ctxt, x, ...)					  \
 	ipc_log_string(ctxt, "[%s]: "x, __func__, ##__VA_ARGS__)
 
@@ -1491,6 +1496,9 @@ static int qcom_glink_native_rx(struct qcom_glink *glink, int iterations)
 		should_wake = false;
 		log_abnormal_wakeup_reason("IRQ %d, %s", glink->irq, glink->irqname);
 		pm_system_wakeup();
+#if IS_ENABLED(CONFIG_SEC_PM)
+		log_suspend_abort_reason(glink->irqname);
+#endif
 	}
 
 	spin_lock_irqsave(&glink->irq_lock, flags);
